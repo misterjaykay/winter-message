9afbf2141ef0b35ee61bb77cd221646dea14d414
Jonghyun Kang <misterjaykay@gmail.com>
2022-12-19 21:07:29 -0600

    Snow commit.

---
+++ pages/component/Snow.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Snow(props) {
+    var numb = 199;
+    var arr = [];
+    for (let i = 0; i < numb; i++) {
+        arr.push(numb[i]);
+    }
+    return (
+        <>
+        {arr.map(e => (
+            <div className="snow"></div>
+        ))}
+        </>
+    )
+}
